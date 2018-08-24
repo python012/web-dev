03332549d9a59ee806338de8187eec63d30c1ca9
Reed Xia <huaqin2005@gmail.com>
2018-08-24 17:43:39 +0800

    learn javascript

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+var userFirstName = prompt("What's your first name?");
+var userLastName = prompt("What's your last name?");
+console.log("Your full name is " + userFirstName + " " + userLastName);
+var userAge = prompt("How old are you?");
+console.log("Your age is " + userAge);
