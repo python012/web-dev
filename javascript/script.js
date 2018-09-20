16484b7a760ed42fb5ace77c6b14d02185d565dc
Reed Xia <huaqin2005@gmail.com>
2018-09-20 17:15:06 +0800

    add a html, manipulate DOM

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,15 @@ var userLastName = prompt("What's your last name?");
 console.log("Your full name is " + userFirstName + " " + userLastName);
 var userAge = prompt("How old are you?");
 console.log("Your age is " + userAge);
+
+
+function f(num) {
+  if (num == 0) {
+    return 1;
+  } else {
+    for (var i=num; i>1; i--) {
+      num = num * (i-1);
+    }
+    return num;
+  }
+}
